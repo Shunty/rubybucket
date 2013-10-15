ad3d1afcb91b67bb5e58bad76cd0d647355f2655
Shunty <shunty21t@gmail.com>
2013-10-15 20:42:38 +0430

    ACTIVATE

--- Convert.rb
+++ Convert.rb
@@ -1,5 +1,6 @@
 def convert(temp_in_fahrenheit)
 	temp_in_celcius = 5.0 / 9 * (temp_in_fahrenheit - 32)
 end
+
 temp_in_fahrenheit = gets.to_f
 puts format("%.2f", convert(temp_in_fahrenheit))
--- SecsInAYear.rb
+++
@@ -1,4 +0,0 @@
-age_in_secs = 979000000
-secs_in_year = 60.0 * 60 * 24 * 365
-age_in_years = age_in_secs / secs_in_year
-puts 'You are ' + format("%.2f", age_in_years) + ' years old'
